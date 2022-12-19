8f9aa28af80278c8bca56946483820c02da4bee5
Scott Pickett <scottpickett@gmail.com>
2022-12-19 14:42:04 -0700

    removing unused 'redirect' import from Flask

--- flask/app/app.py
+++ flask/app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, render_template, request, url_for
 import app.server as server
 
 app = Flask(__name__)
